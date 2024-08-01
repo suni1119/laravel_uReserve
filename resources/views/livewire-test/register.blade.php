299501d1a1f7f5238d947e41c7b9c9a78ceed3fa
suni1119 <junsannnnn1119@gmail.com>
2024-08-02 07:03:07 +0900

    livewire test

---
+++ resources/views/livewire-test/register.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
+        @livewireStyles
+    </head>
+    <body>
+        livewireテスト <span class="text-blue-600">register</span>
+        @livewire('register')
+        @livewireScripts
+    </body>
+</html>
