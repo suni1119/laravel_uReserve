6d22bc43d6edf2443e5ea26961d4e0008a568a2f
suni1119 <junsannnnn1119@gmail.com>
2024-08-25 05:57:43 +0900

    event dummy 修正

---
+++ resources/views/livewire/calendar.blade.php
@@ -0,0 +1,16 @@
+<div>
+    カレンダー
+    <x-jet-input id="calendar" class="block mt-1 w-full" 
+    type="text" name="calendar" 
+    value="{{ $currentDate }}"
+    wire:change="getDate($event.target.value)"/>
+    {{ $currentDate }}
+    <div class="flex">
+        @for($day = 0; $day < 7; $day++)
+            {{ $currentWeek[$day] }}
+        @endfor
+    </div>
+    @foreach ((array)$events as $event)
+    {{ $event->start_date }}
+    @endforeach
+</div>
