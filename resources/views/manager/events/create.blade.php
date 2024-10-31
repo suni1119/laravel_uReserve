f25bfed23eb3950e7c0e9d4f25658e2ee879ffab
suni1119 <junsannnnn1119@gmail.com>
2024-10-31 21:08:48 +0900

    1人当たりの単価をビュー側で表示させる

--- resources/views/manager/events/create.blade.php
+++ resources/views/manager/events/create.blade.php
@@ -53,8 +53,8 @@
                             </div>
 
                             <div class="mt-4">
-                                <x-jet-label for="price" value="金額" />
-                                <x-jet-input id="price" class="block mt-1 w-full" type="number" name="price" step="100" />
+                                <x-jet-label for="unit_price" value="1人当たりの単価" />
+                                <x-jet-input id="unit_price" class="block mt-1 w-full" type="number" name="unit_price" step="100" required/>
                             </div>
 
                             <div class="flex space-x-4 justify-around">
